79daa50454a60082be4d0bf9418830106af3394f
Mateus Auler <mateusauler@protonmail.com>
2020-07-14 10:06:19 -0300

    Patched message

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ### Currently applied patches:
 
 - Dpms
+- Message
--- config.def.h
+++ config.def.h
@@ -13,3 +13,12 @@ static const int failonclear = 0;
 
 /* time in seconds before the monitor shuts down */
 static const int monitortime = 5;
+
+/* default message */
+static const char * message = "I'm locked!";
+
+/* text color */
+static const char * text_color = "#AAAAAA";
+
+/* text size (must be a valid size) */
+static const char * font_name = "-adobe-helvetica-medium-r-normal--34-240-100-100-p-176-iso10646-1";
